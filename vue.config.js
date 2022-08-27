0005f632708f03d2ee41f1e842ff82177bea318b
aydar.mazitov <aydar.mazitov@gmail.com>
2022-08-27 22:37:31 +0500

    Config

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue_weather_widget/'
-    : '/'
-  //publicPath: '/vue_weather_widget',
+  publicPath: '/vue_weather_widget',
   
   
 })
